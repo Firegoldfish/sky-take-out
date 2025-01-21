d040ebfc19eaaab77e98e48166f4bb5c032f1904
Bill Madison <Firegoldfish@outlook.com>
2025-01-21 23:48:08 +0800

    分类查询

--- sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -1,16 +1,15 @@
 package com.sky.controller.admin;
 
 import com.sky.dto.CategoryDTO;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.CategoryService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -33,5 +32,11 @@ public class CategoryController {
         categoryService.save(categoryDTO);
         return Result.success();
     }
-
+    @GetMapping("/page")
+    @ApiOperation(value = "分页查询分类")
+    public Result<PageResult> page(CategoryPageQueryDTO categoryPageQueryDTO) {
+        log.info("分页查询分类：{}", categoryPageQueryDTO);
+        PageResult pageResult =  categoryService.pageQuery(categoryPageQueryDTO);
+        return Result.success(pageResult);
+    }
 }
--- sky-server/src/main/java/com/sky/service/impl/CategoryServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/CategoryServiceImpl.java
@@ -1,10 +1,14 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.constant.StatusConstant;
 import com.sky.context.BaseContext;
 import com.sky.dto.CategoryDTO;
+import com.sky.dto.CategoryPageQueryDTO;
 import com.sky.entity.Category;
 import com.sky.mapper.CategoryMapper;
+import com.sky.result.PageResult;
 import com.sky.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
@@ -23,14 +27,21 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     @Override
-        public void save(CategoryDTO categoryDTO) {
-            Category category = new Category();
-            BeanUtils.copyProperties(categoryDTO, category);
-            category.setStatus(StatusConstant.DISABLE);
-            category.setCreateTime(LocalDateTime.now());
-            category.setUpdateTime(LocalDateTime.now());
-            category.setCreateUser(BaseContext.getCurrentId());
-            category.setUpdateUser(BaseContext.getCurrentId());
-            categoryMapper.insert(category);
-        }
+    public void save(CategoryDTO categoryDTO) {
+        Category category = new Category();
+        BeanUtils.copyProperties(categoryDTO, category);
+        category.setStatus(StatusConstant.DISABLE);
+        category.setCreateTime(LocalDateTime.now());
+        category.setUpdateTime(LocalDateTime.now());
+        category.setCreateUser(BaseContext.getCurrentId());
+        category.setUpdateUser(BaseContext.getCurrentId());
+        categoryMapper.insert(category);
+    }
+
+
+    public PageResult pageQuery(CategoryPageQueryDTO categoryPageQueryDTO) {
+        PageHelper.startPage(categoryPageQueryDTO.getPage(), categoryPageQueryDTO.getPageSize());
+        Page<Category> categoryPage = categoryMapper.pageQuery(categoryPageQueryDTO);
+        return new PageResult(categoryPage.getTotal(), categoryPage.getResult());
+    }
 }
