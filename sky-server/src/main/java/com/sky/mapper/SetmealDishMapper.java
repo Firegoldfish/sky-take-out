1cf63f8112b957d5d269b6936e92fca844c68a0b
Bill Madison <Firegoldfish@outlook.com>
2025-01-31 00:27:43 +0800

    新增套餐

--- sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
@@ -1,5 +1,6 @@
 package com.sky.mapper;
 
+import com.sky.entity.SetmealDish;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -11,4 +12,7 @@ public interface SetmealDishMapper {
     //根据菜品id查询套餐id
     // @Select("select setmeal_id from setmeal_dish where dish_id = #{dishId}")
     List<Long> getSetmealIdsByDishIds(List<Long> dishIds);
+
+
+    void insertBatch(List<SetmealDish> setmealDishes);
 }
--- sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -1,11 +1,51 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.entity.Setmeal;
+import com.sky.entity.SetmealDish;
+import com.sky.mapper.SetmealDishMapper;
+import com.sky.mapper.SetmealMapper;
+import com.sky.result.PageResult;
 import com.sky.service.SetmealService;
+import com.sky.vo.SetmealVO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
 public class SetmealServiceImpl implements SetmealService{
 
+    private final SetmealMapper setmealMapper;
+    private final SetmealDishMapper setmealDishMapper;
+
+    public SetmealServiceImpl(SetmealMapper setmealMapper, SetmealDishMapper setmealDishMapper) {
+        this.setmealMapper = setmealMapper;
+        this.setmealDishMapper = setmealDishMapper;
+    }
+
+    @Override
+    public void saveWithDish(SetmealDTO setmealDTO) {
+        Setmeal setmeal = new Setmeal();
+        BeanUtils.copyProperties(setmealDTO, setmeal);
+        setmealMapper.insert(setmeal);
+        Long setmealId = setmeal.getId();
+        List<SetmealDish> setmealDishes = setmealDTO.getSetmealDishes();
+        setmealDishes.forEach(sd -> sd.setSetmealId(setmealId));
+        setmealDishMapper.insertBatch(setmealDishes);
+        log.info("保存套餐以及菜品:{}", setmealDTO);
+    }
+
+    @Override
+    public PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO) {
+        PageHelper.startPage(setmealPageQueryDTO.getPage(), setmealPageQueryDTO.getPageSize());
+        Page<Setmeal> setmealPage = setmealMapper.pageQuery(setmealPageQueryDTO);
+        return new PageResult(setmealPage.getTotal(), setmealPage.getResult());
+    }
 }
