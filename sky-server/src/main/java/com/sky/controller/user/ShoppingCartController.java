c778671d323286c6642404b70e0f904cdbf8df04
Bill <Firegoldfish@outlook.com>
2025-02-11 18:16:16 +0800

    购物车

---
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -0,0 +1,31 @@
+package com.sky.controller.user;
+
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.result.Result;
+import com.sky.service.ShoppingCartService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/user/shoppingCart")
+@RestController
+@Slf4j
+@Api(tags = "C端-购物车相关接口")
+public class ShoppingCartController {
+
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+    @PostMapping("/add")
+    @ApiOperation("添加购物车")
+    public Result add(@RequestBody ShoppingCartDTO shoppingCartAddDTO) {
+        log.info("添加购物车：{}", shoppingCartAddDTO);
+        shoppingCartService.addShoppingCart(shoppingCartAddDTO);
+        return Result.success();
+    }
+}
