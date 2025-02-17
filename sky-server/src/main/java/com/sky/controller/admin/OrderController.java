83301981690dc1650a214c3e6df630208dd0cb17
bill <firegoldfish@outlook.com>
2025-02-17 15:20:38 +0800

    订单管理-修正

--- sky-server/src/main/java/com/sky/controller/admin/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -7,6 +7,7 @@ import com.sky.service.OrderService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @Api(tags = "订单管理接口")
 public class OrderController {
+    @Autowired
     private OrderService orderService;
 
     @GetMapping("/conditionSearch")
     @ApiOperation("订单搜索")
     public Result<PageResult> conditionSearch(OrdersPageQueryDTO ordersPageQueryDTO){
+        log.info("订单搜索:{}", ordersPageQueryDTO);
         PageResult pageResult = orderService.conditionSearch(ordersPageQueryDTO);
         return Result.success(pageResult);
     }
